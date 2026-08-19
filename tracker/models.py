88639b46f27600542fd7dc1f7b0b0292792bd560
Mohlaroy <mganijonova861@gmail.com>
2026-08-19 12:20:52 +0500

    Added core coding session outlook

---
+++ tracker/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class Technology(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class CodingSession(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    duration_minutes = models.DurationField()
+    technologies = models.ManyToManyField(Technology, related_name="sessions", blank=True,)
+    github_commit = models.CharField(max_length=100, blank=True,)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.date}"
+
