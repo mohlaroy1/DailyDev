c33db863515b25c9c063583af82610fca1e880d7
Mohlaroy <mganijonova861@gmail.com>
2026-08-21 16:08:29 +0500

    Admin register

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Technology, CodingSession 
 
-# Register your models here.
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    search_fields = ("name", "category")
+    list_filter = ("category",)
+
+
+
+@admin.register(CodingSession)
+class CodingSessionAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "date", "duration_minutes", "created_at",)
+    list_filter = ("date", "technologies")
+    search_fields = ("title", "description", "user__username",)
+    filter_horizontal = ("technologies",)
+    ordering = ("-date", "-created_at")
