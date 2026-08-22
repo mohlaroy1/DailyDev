2ea7898eadcb49771b98facf0b8b2546264e5de4
Mohlaroy <mganijonova861@gmail.com>
2026-08-22 23:28:04 +0500

    Day 6: Add coding session list page

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import CodingSession
 
-# Create your views here.
+
+def session_list(request):
+    sessions = (
+        CodingSession.objects
+        .select_related("user")
+        .prefetch_related("technologies")
+        .order_by("-date", "-created_at")
+    )
+
+    return render(
+        request,
+        "tracker/session_list.html",
+        {"sessions": sessions},
+    )
